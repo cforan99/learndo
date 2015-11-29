9fbee6b66541c37c18a8ad3461b6e7a669790fb8
Christina Foran <cforan99@gmail.com>
2015-11-29 03:01:03 -0800

    updated tests and added readme

---
+++ README.rst
@@ -0,0 +1,35 @@
+================================================
+`Learndo <a href=http://learndo.herokuapp.com>`_
+================================================
+
+Learndo is an assignment portal that makes it easy for teachers to share links to online resources and monitor their students' progress.
+
+Features:
+---------
+
+- Different views and permissions for teacher and student accounts
+- Teachers can create student accounts and organize them into classes.
+- Students' assignment list updates as soon as a new task is assigned.
+- Assignments can be sorted by date and filtered by status.
+- Students can check off an assignment when completed.
+- In the teacher's view, the list of the students' statuses updates in realtime.
+- The status of each assignment is color coded throughout the UI.
+
+Tech Stack:
+-----------
+
+- Python
+- SQLite & PostgreSQL relational databases
+- SQLAlchemy Object Relational Mapper
+- Flask web framework
+- Jinja templating
+- Javascript
+- jQuery
+- AJAX 
+- JSON
+- CSS + Bootstrap
+- Testing with unittest and Selenium WebDriver
+- Deployed on Heroku
+
+
+
--- tests.py
+++ tests.py
@@ -5,9 +5,10 @@ class TestClassList(unittest.TestCase):
     """Function tests for the forms on the class lists page"""
 
     def setUp(self):
+        """Open webdriver and sign in"""
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(10)
-        self.browser.get('http://localhost:5000/login')
+        self.browser.get('http://localhost:5000/')
         username = self.browser.find_element_by_name('username')
         password = self.browser.find_element_by_name('password')
         btn = self.browser.find_element_by_id("login-button")
@@ -17,25 +18,46 @@ class TestClassList(unittest.TestCase):
         btn.click()
 
     def tearDown(self):
+        """Sign out and close browser"""
         self.browser.get('http://localhost:5000/logout')
         self.browser.quit()
 
     def test_did_display(self):
+        """Checks to see if the class list loaded."""
         self.browser.get('http://localhost:5000/teacher/0/classes')
-        heading = self.browser.find_element_by_id("your-classes")
-        self.assertIn("Your classes", heading.text)
+        heading = self.browser.find_element_by_id("my-classes")
+        self.assertIn("My classes", heading.text)
 
-    def test_create_class(self):
-        """After adding, if the class name appears on the page, it has been added to the db."""
+    def test_create_remove_class(self):
+        """After adding and refreshing, if the class name appears on the page, it has been added to the db.
+        After clicking the delete button and the confirmation button in the pop up menu, a flash message should
+        appear if the class has been deleted from the db."""
         self.browser.get('http://localhost:5000/teacher/0/classes')
+        btn = self.browser.find_element_by_id('new-class')
+        btn.click()
+
         textfield = self.browser.find_element_by_name('class_name')
         textfield.send_keys("Test Class")
 
-        submit = self.browser.find_element_by_id("create-class")
+        submit = self.browser.find_element_by_id("new-class-button")
         submit.click()
+
+        self.browser.refresh()
 
         listing = self.browser.find_element_by_id("class-list")
         self.assertIn("Test Class", listing.text)
+
+        # remove = self.browser.find_element_by_xpath("#class-list ul li:first button")
+        # remove.click()
+
+        # confirm = self.browser.find_element_by_id("remove")
+        # confirm.click()
+
+        # self.browser.refresh()
+
+        # flash = self.browser.find_element_by_id("flash")
+        # self.assertIn("The class, Test Class, has been deleted", flash.text)
+
 
     """Remove student / class"""
 
