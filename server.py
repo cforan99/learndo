fc37ede1b54bbf60ef33eaeb75ccad2b2ea420d1
Christina Foran <cforan99@gmail.com>
2015-11-28 19:26:03 -0800

    tweaking debug mode

--- server.py
+++ server.py
@@ -700,8 +700,7 @@ def assign_to_class():
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
-    app.debug = True
-
+    
     connect_to_db(app)
 
     # Use the DebugToolbar
